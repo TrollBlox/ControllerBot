90db68e87d4a521954d54892eae066c24a709507
TrollBlox <caelbrenning@gmail.com>
2022-10-17 21:20:15 -0500

    updated .gitignore

---
+++ commands/timeout.js
@@ -0,0 +1,53 @@
+const { SlashCommandBuilder, EmbedBuilder, Colors, PermissionsBitField } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('timeout')
+    .setDescription('Timeouts a user')
+    .addUserOption(option =>
+      option.setName('user')
+      .setDescription('The user to timeout')
+      .setRequired(true))
+    .addIntegerOption(option =>
+      option.setName('length')
+      .setDescription('Minutes to timeout a user')
+      .setRequired(true))
+    .addStringOption(option =>
+      option.setName('reason')
+      .setDescription('The reason for the timeout')
+      .setRequired(false)),
+    async execute(int) {
+      const user = await int.options.getUser('user');
+      const time = await int.options.getInt('length');
+      const reason = await int.options.getString('reason') || 'No reason provided';
+      const embed = new EmbedBuilder();
+      await embed.setColor(Colors.Blurple);
+
+      if (!(int.guild.members.fetch(int.author).permissions.has( [ PermissionsBitField.Flags.BanMembers ] ))) {
+        await embed.setTitle('Error');
+        await embed.setDescription(`You cannot ban members!`);
+        return await int.reply({ embeds: [embed] });
+      }
+
+      if (int.member.roles.highest.comparePositionTo(int.guild.members.cache.get(user.id).roles.highest) < 1) {
+        await embed.setTitle('Error');
+        await embed.setDescription(`You cannot ban <@${user.id}>!`);
+        return await int.reply({ embeds: [embed] });
+      }
+
+      if (!int.guild.members.fetch(user).bannable) {
+        await embed.setTitle('Error');
+        await embed.setDescription(`<@${user.id}> is not bannable by the bot!`);
+        return await int.reply({ embeds: [embed] });
+      }
+
+      await embed.setTitle('Ban');
+      await embed.setDescription(`You have been banned from ${int.guild.name} for ${reason}!`);
+      await user.createDM();
+      await user.send({ embeds: [ embed ] } );
+      await int.guild.members.fetch(user).ban({ reason: reason });
+      await embed.setDescription(`<@${user.id}> has been banned for ${reason}!`);
+      return await int.reply({ embeds: [embed] });
+
+    }
+};
